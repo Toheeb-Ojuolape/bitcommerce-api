79b8213ca4862f368af4ff5268f0d7fdfae3a2a6
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-08-17 10:39:38 +0100

    feat: added node version

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const express = require("express");
+import { Request,Response } from "express";
 import router from "./src/routes/routes";
 import { Socket } from "socket.io";
 const cors = require("cors");
@@ -19,6 +20,11 @@ app.use(express.json());
 const server = app.listen(port, () => {
   console.log(`App listening on PORT: ${port}`);
 });
+
+
+app.get("/",(req:Request,res:Response)=>{
+  res.send(`<h2>Bitcommerce API</h2>`)
+})
 
 const io = require("socket.io")(server, {
   cors: {
